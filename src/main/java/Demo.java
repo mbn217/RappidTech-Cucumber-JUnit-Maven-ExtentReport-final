524a3798b6a8bc233c6184ed2feaa029b2395c66
Mohamed Nheri <nherimohamed@gmail.com>
2024-04-01 22:36:55 -0400

    showing system property examples

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -16,6 +16,14 @@ public class Demo {
             System.out.println(" ========     >>>>>>   "+name);
         }
 
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.version"));
+        System.out.println(System.getProperty("user.home"));
+
+
+
+
+
 
 
     }
